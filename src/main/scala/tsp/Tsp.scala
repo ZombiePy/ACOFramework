c7e1e775f476f1372f825930749eb3f6c3d5def8
Tymoteusz Dobrzański <tdobrzanski97@gmail.com>
2022-02-03 23:51:30 +0100

    Tsp update

--- src/main/scala/tsp/Tsp.scala
+++ src/main/scala/tsp/Tsp.scala
@@ -1,5 +1,9 @@
 package tsp
 
+import project.graph.Node
+import project.problem.TspProblem
+import tsp.nodeCoordSectionToMatrix
+
 sealed trait TspType
 case object TSPTspType extends TspType
 
@@ -15,3 +19,21 @@ case class Tsp(
     edgeWeightType: EdgeWeightType,
     nodeCoordSection: Map[CityName, (Double, Double)]
 )
+
+def TspToProblem(tsp: Tsp): (Map[Node, CityName], TspProblem) = {
+  val citiesEnumerated = tsp.nodeCoordSection
+    .map(_._1)
+    .zipWithIndex
+    .map((name, index) => (name, Node(index)))
+    .toMap
+
+  val mapped = tsp.nodeCoordSection.map(key_value =>
+      (citiesEnumerated(key_value._1), key_value._2)
+    )
+
+  val tspProblem = TspProblem(
+    nodes = mapped.keys.toList,
+    matrix = nodeCoordSectionToMatrix(mapped)
+  )
+  (citiesEnumerated.map(_.swap).toMap, tspProblem)
+}
