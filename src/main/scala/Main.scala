2734d1b3291683f1c61db9a698978d73c4d4a271
Vergenter <okolok1998@gmail.com>
2022-02-04 12:07:30 +0100

    read problem type from config.yaml

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,6 +11,7 @@ import tsp.TspToProblem
 import tsp.Tsp
 import project.algorithm.BasicAlgorithm
 import project.algorithm.TspSolver
+import project.algorithm.BaseAlgorithm
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -18,24 +19,45 @@ object Main {
     val filename = "src//main//config.yaml"
     val input = new FileInputStream(new File(filename))
     val yaml = new Yaml(new Constructor(classOf[ProblemConfig]))
-    val e = yaml.load(input).asInstanceOf[ProblemConfig]
-    val tsps = for {
-      file <- asScalaBuffer(e.problem)
-    } yield {
-      TspReader.read(new File("src//main//resources//" ++ file))
-    }
-//    val (reverseNameMap, mtsp) = TspsToMtsp(tsps)
-    val (reverseNameMap, tsp) = TspToProblem(tsps.head)
     val ants_number = 100
     val algorithm_iterations = 100
-//    val algo = BasicAlgorithm(ants_number, mtsp, algorithm_iterations)
-    val algo = TspSolver(ants_number, tsp, algorithm_iterations)
-    algo.run()
+    val e = yaml.load(input).asInstanceOf[ProblemConfig]
+    e.problemType match {
+      case "tsp" => {
+        val tsp = TspReader.read(
+          new File(
+            "src//main//resources//" ++ asScalaBuffer(e.problemFiles).head
+          )
+        )
+        val (reverseNameMap, tspProblem) = TspToProblem(tsp)
+        val algo = TspSolver(ants_number, tspProblem, algorithm_iterations)
+        algo.run()
+      }
+      case "mtsp" => {
+        val tsps = for {
+          file <- asScalaBuffer(e.problemFiles)
+        } yield {
+          TspReader.read(new File("src//main//resources//" ++ file))
+        }
+        val (reverseNameMap, mtspProblem) = TspsToMtsp(tsps)
+        val algo =
+          BasicAlgorithm(ants_number, mtspProblem, algorithm_iterations)
+        algo.run()
+      }
+      case _ =>
+        throw NotImplementedError(
+          "Your method from config.yaml is not implemented!"
+        )
+    }
+
+//
+
   }
 }
 class ProblemConfig {
-  @BeanProperty var problem = new java.util.ArrayList[String]()
+  @BeanProperty var problemType: String = null
+  @BeanProperty var problemFiles = new java.util.ArrayList[String]()
   override def toString: String = {
-    return String.join(",", problem)
+    return String.join(",", problemFiles)
   }
 }
